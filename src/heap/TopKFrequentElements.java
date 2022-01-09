ae4ad1a89ddd08845f4b659b0102395dc00eeab4
dindina <dindina@gmail.com>
2022-01-09 14:33:06 -0600

    tree traversal

--- src/heap/TopKFrequentElements.java
+++ src/heap/TopKFrequentElements.java
@@ -1,6 +1,8 @@
 package heap;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 
@@ -13,9 +15,19 @@ public class TopKFrequentElements {
         System.out.println("Here are the top K numbers: " + result);
     }
 
-    private static List<Integer> find(final int[] nums, final int i) {
+    private static List<Integer> find(final int[] nums, final int k) {
+
+        Map<Integer,Integer> frequencyMap = new HashMap<>();
+
+        for(int i=0; i < nums.length ; i++)
+        {
+            frequencyMap.put(i, frequencyMap.getOrDefault(i,0)+1);
+        }
+        System.out.println(frequencyMap);
 
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((n1,n2)-> n1-n2);
+
+
         return null;
     }
 
