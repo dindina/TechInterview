bd2cdcb7d35c58d15c2a8a96b782268c76a2cde5
dinesh <dindina@gmail.com>
2024-05-01 20:12:06 -0500

    dutch national flag

--- src/twopointers/RemoveDuplicatesFromArrayInPlace.java
+++ src/twopointers/RemoveDuplicatesFromArrayInPlace.java
@@ -1,5 +1,13 @@
 package twopointers;
 
+/**
+ * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+ *
+ * Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+ *
+ * Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+ * Return k.
+ */
 public class RemoveDuplicatesFromArrayInPlace {
 
     public static void main(String[] args) {
@@ -15,15 +23,15 @@ public class RemoveDuplicatesFromArrayInPlace {
 
     private static int removeDuplicates(int[] nums) {
 
-        int j = 0;
-        int n = nums.length;
-        for (int i = 1; i < n; i++) {
-
-            if (nums[i] != nums[j]) {
-                nums[++j]=nums[i];
+        int slowPointer = 0;
+        int fastPointer = 0;
+        while (fastPointer < nums.length) {
+            if (nums[slowPointer] != nums[fastPointer]) {
+                nums[++slowPointer] = nums[fastPointer];
             }
+            fastPointer++;
         }
-        return j+1;
+        return slowPointer + 1;
     }
 
 
